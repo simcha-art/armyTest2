95fc2e64d996335444c8126bf6a853f615fc3f17
simcha <simchacop@gmail.com>
2026-08-24 18:06:22 +0300

    feat: add one test

--- server/service/tests/service.test.js
+++ server/service/tests/service.test.js
@@ -1,9 +1,17 @@
-import {describe, it} from "node:test"
+import {describe, it, mock} from "node:test"
 import assert from "node:assert/strict"
 import { mockTerritories } from "./mocks.js"
+import { computerReinforce } from "../../helper/logics.js"
 
 describe("check adding soldiers to beirut", () => {
     it("zidon's owner is the player, with 10 soldiers, should add 3 sodleirs to beirut", () => {
+        let mock1 = mockTerritories
+        mock1[3].owner = "player"
+        mock1[3].soldiers = 10
+        const {territories, toId} = computerReinforce(mock1)
+        let expected = mock1
+        expected[0].soldiers += 3
+        assert.deepEqual(territories, expected )
 
     })
 })
