cbef50482f5c588c4a66a7d7eb1a9a585ce63156
simcha <simchacop@gmail.com>
2026-08-24 17:59:06 +0300

    feat: add basis for tests

---
+++ server/service/tests/service.test.js
@@ -0,0 +1,9 @@
+import {describe, it} from "node:test"
+import assert from "node:assert/strict"
+import { mockTerritories } from "./mocks.js"
+
+describe("check adding soldiers to beirut", () => {
+    it("zidon's owner is the player, with 10 soldiers, should add 3 sodleirs to beirut", () => {
+
+    })
+})
